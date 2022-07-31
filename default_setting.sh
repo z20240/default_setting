1f09bdeb182b4c445056b5250bb0f6ed410e7511
les.lee <11765954+z20240@users.noreply.github.com>
2022-07-31 18:28:13 +0800

    Update default_setting.sh

--- default_setting.sh
+++ default_setting.sh
@@ -41,7 +41,7 @@ brew install autojump
 ### ./install.py
 
 # syntax highlight
-git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 source ~/.zshrc
 # --------------------------------------
